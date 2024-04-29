0c5e353c914885a233cfbef001c0304527e4e779
ImaneKr <imanekari1313@gmail.com>
2024-04-30 01:21:51 +0200

    idk just to updated

---
+++ backend/models/kid.js
@@ -0,0 +1,52 @@
+// models/kid.js
+
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const Kid = sequelize.define('Kid', {
+  kid_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  firstname: {
+    type: DataTypes.STRING(100),
+    allowNull: false
+  },
+  lastname: {
+    type: DataTypes.STRING(50)
+  },
+  gender: {
+    type: DataTypes.ENUM('Male', 'Female', 'Other'),
+    allowNull: false
+  },
+  allergies: {
+    type: DataTypes.JSON
+  },
+  hobbies: {
+    type: DataTypes.JSON
+  },
+  syndroms: {
+    type: DataTypes.JSON
+  },
+  prof_time_creation: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+  prof_pic: {
+    type: DataTypes.STRING(250)
+  },
+  guardian_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  category_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'category',
+      key: 'category_id'
+    }
+  }
+});
+
+module.exports = Kid;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,33 @@
 const express = require('express');
-const pool = require('./db');
-const kid = require('./routes/kids');
-const guardian = require('./routes/guardian');
+const {Sequelize} =require('sequelize');
 
+const config = require('./config/config.json')[process.env.NODE_ENV || 'development'];
 
 const app = express();
-const PORT = 3000;
 
-app.listen(PORT ,()=>{
-  console.log(`server is running on port ${PORT}`);
+const sequelize = new Sequelize(config.databse, config.username ,config.password, {
+  host : config.host,
+  port: config.port,
+  dialect: config.dialect,
+  logging: config.logging
 });
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log('Database synchronized');
+  })
+  .catch(err => {
+    console.error('Error synchronizing database:', err);
+  });
+
+sequelize.authenticate()
+  .then(()=>{
+    console.log('Database connection established successfully');
+  })
+  .catch(err =>{
+    console.error('Unable to connect to the database: ',err);
+  });
+
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT ,() =>{
+    console.log(`Server is running on port ${PORT}`);
+  })
