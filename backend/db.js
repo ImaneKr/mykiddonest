eb33eb5cc5a55088ce286994d52557ee0cffab3a
ImaneKr <imanekari1313@gmail.com>
2024-04-29 20:57:18 +0200

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: 'mysql-cf7e496-mykiddonest-caac.j.aivencloud.com',
-  port: '25298',
-  database: 'defaultdb',
-  user: 'avnadmin',
+  host: process.env.DB_HOST,
+  port: process.env.PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
 });
 
