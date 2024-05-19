021fe8521b65e10167993c50878e132ec53c1cbd
Boucham Marwa <marwa.boucham@univ-tlemcen.dz>
2024-05-19 16:16:39 +0100

    commit 9 (integrating payement)

--- appmobile/lib/view/screens/payment.dart
+++ appmobile/lib/view/screens/payment.dart
@@ -1,6 +1,13 @@
 // ignore_for_file: prefer_const_constructors
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:appmobile/controller/payment_controller.dart';
+import 'package:appmobile/models/payment.dart';
 import 'package:appmobile/view/components/paymentTextField.dart';
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Payment extends StatefulWidget {
   const Payment({Key? key}) : super(key: key);
@@ -13,26 +20,95 @@ class _Payment extends State<Payment> {
   bool mealIsChecked = false;
   bool deliveryIsChecked = false;
 
-  String selectedOption = 'edahabia';
-
   Image imageEdahabia = Image.asset('assets/images/edahabia3.png');
   Image imageCib = Image.asset('assets/images/cib.png');
+
+  final guardian_id = Hive.box('guardianData').get('guardian_id');
+  final kid_id = Hive.box('selectedKid').get('selectedKid')['kid_id'];
 
   final TextEditingController firstNameController = TextEditingController();
   final TextEditingController lastNameController = TextEditingController();
   final TextEditingController phoneNumberController = TextEditingController();
-  final TextEditingController adressController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController addressController = TextEditingController();
   final TextEditingController countryController = TextEditingController();
   final TextEditingController stateController = TextEditingController();
 
+  @override
   void dispose() {
-    // Dispose of the controllers when the widget is removed
     firstNameController.dispose();
     lastNameController.dispose();
     phoneNumberController.dispose();
+    emailController.dispose();
+    addressController.dispose();
     countryController.dispose();
     stateController.dispose();
     super.dispose();
+  }
+
+  final PaymentController paymentController = PaymentController();
+  int totalAmount = 4000;
+  int calculateTotalAmount() {
+    int baseAmount = 4000;
+    int mealAmount = 500;
+    int deliveryAmount = 500;
+
+    int calculatedAmount = baseAmount;
+
+    if (mealIsChecked) {
+      calculatedAmount += mealAmount;
+    }
+
+    if (deliveryIsChecked) {
+      calculatedAmount += deliveryAmount;
+    }
+
+    return calculatedAmount;
+  }
+
+  void updateTotalAmount() {
+    setState(() {
+      totalAmount = calculateTotalAmount();
+    });
+  }
+
+  Future processPayment() async {
+    try {
+      // Create a customer first
+      await paymentController
+          .createCustomer(
+        name: firstNameController.text + ' ' + lastNameController.text,
+        email: emailController.text,
+        phone: phoneNumberController.text,
+        state: stateController.text,
+        address: addressController.text,
+      )
+          .then((value) async {
+        log("customer id is " + value.toString());
+        PaymentModel paymentModel = PaymentModel(
+          amount: totalAmount,
+          guardian_id: guardian_id,
+          kid_id: kid_id,
+          customerId: value,
+        );
+
+        final checkoutData =
+            await paymentController.createCheckout(paymentModel);
+
+        final checkoutUrl = checkoutData['checkout_url'];
+
+        await launchUrl(Uri.parse(checkoutUrl));
+      });
+    } catch (e) {
+      log(e.toString());
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Error'),
+          content: Text('An error occurred while processing the payment.'),
+        ),
+      );
+    }
   }
 
   @override
@@ -124,7 +200,7 @@ class _Payment extends State<Payment> {
                       width: 140.0,
                     ),
                     Text(
-                      '4000.0DA',
+                      ' $totalAmount DZD',
                       style: TextStyle(
                         fontSize: 18,
                         fontFamily: 'Inter',
@@ -253,7 +329,7 @@ class _Payment extends State<Payment> {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          'Adress',
+                          'Email',
                           style: TextStyle(
                             color: Color(0xFF01448B),
                             fontSize: 16,
@@ -265,8 +341,35 @@ class _Payment extends State<Payment> {
                           height: 5.0,
                         ),
                         PaymentTextField(
-                          controller: adressController,
-                          labelText: 'adress..',
+                          controller: emailController,
+                          labelText: 'example@gmail.com',
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Enter your email adress';
+                            }
+                            return null;
+                          },
+                        ),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Address',
+                          style: TextStyle(
+                            color: Color(0xFF01448B),
+                            fontSize: 16,
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        PaymentTextField(
+                          controller: addressController,
+                          labelText: 'address..',
                           validator: (value) {
                             if (value == null || value.isEmpty) {
                               return 'Enter your adress';
@@ -429,7 +532,10 @@ class _Payment extends State<Payment> {
                     ),
                   ),
                   onPressed: () {
-                    if (_formKey.currentState!.validate()) {}
+                    if (_formKey.currentState!.validate()) {
+                      updateTotalAmount();
+                      processPayment();
+                    }
                   },
                   child: Text(
                     'Proceed payment',
